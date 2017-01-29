9b9ab87bcaf52b1e01924f92beca77f577dd2bf1
hassony2 <yanahasson@gmail.com>
2017-01-29 17:28:42 +0100

    Add game class and basic gitignore

---
+++ pac_man/game.py
@@ -0,0 +1,72 @@
+from time import sleep
+
+from matplotlib import pyplot as plt
+
+from tools import timeit
+
+
+class Game:
+    def __init__(self, board, speed, pac_man_agent=None, ghost_agents=None):
+        self.board = board
+        self.pac_man = pac_man_agent
+        self.speed = speed
+        if (not ghost_agents):
+            self.ghosts = []
+        else:
+            self.ghosts = ghost_agents
+
+    def add_pac_man(self, agent):
+        target_position = agent.current_node.position
+        if (target_position in self.board.board_nodes):
+            target_node = self.board.board_nodes[agent.current_node.position]
+            self.pac_man = agent
+        else:
+            raise OutsideOfLegalPath("Cannot add agent\
+                to invalid board position")
+
+    def add_ghost(self, agent):
+        target_position = agent.current_node.position
+        if (target_position in self.board.board_nodes):
+            target_node = self.board.board_nodes[agent.current_node.position]
+            self.ghosts.append(agent)
+        else:
+            raise OutsideOfLegalPath("Cannot add agent\
+                to invalid board position")
+
+    def play_game(self, game_steps):
+        game_reward = 0
+        for step in range(game_steps):
+            self.pac_man.move()
+            game_reward += self.pac_man.current_node.reward
+            self.pac_man.current_node.reward = 0
+            for ghost in self.ghosts:
+                ghost.move()
+
+            board_title = 'reward : ' + str(game_reward)
+            self.draw_state(board_title)
+            sleep(self.speed)
+
+    def compute_state(self):
+        current_board = self.board.board_outline.copy()
+        for position, node in self.board.board_nodes.items():
+            if (node.reward > 0):
+                current_board[node.position[0], node.position[1]] = 4
+        for ghost in self.ghosts:
+            row, col = ghost.get_position()
+            current_board[row, col] = 3
+        pac_man_row, pac_man_col = self.pac_man.get_position()
+        current_board[pac_man_row, pac_man_col] = 2
+        return current_board
+
+    @timeit
+    def draw_state(self, title):
+        current_board = self.compute_state()
+        # plt.figure(1)
+        plt.matshow(current_board, fignum=0)
+        plt.title(title)
+        plt.draw()
+        plt.show(block=False)
+        plt.clf()
+
+    def __str__(self):
+        return str(self.compute_state())
--- pac_man/pacman.py
+++ pac_man/pacman.py
@@ -1,37 +1,29 @@
-from agents import Ghost, PacMan
-from board import Board
 from time import sleep
+
 from tools import timeit
 
+from agents import Ghost, PacMan
+from board import Board
+from game import Game
 
-def play_games(board, game_steps, speed):
-    game_reward = 0
-    for step in range(game_steps):
-        for agent in board.agents:
-            agent.move()
-            if (isinstance(agent, PacMan)):
-                game_reward += agent.current_node.reward
-                agent.current_node.reward = 0
-        board_title = 'reward : ' + str(game_reward)
-        board.draw_board(board_title)
-        sleep(speed)
 
 if __name__ == '__main__':
     new_board = Board('simple_board.txt')
-    # Add pacman
+    game_speed = 0
+    new_game = Game(new_board, game_speed)
+    # Create agents
     pac_man_init_node = new_board.board_nodes[(5, 5)]
     pac_man = PacMan(pac_man_init_node)
 
-    # Add ghosts
     ghost1_init_node = new_board.board_nodes[(0, 0)]
     ghost1 = Ghost(ghost1_init_node)
     ghost2_init_node = new_board.board_nodes[(0, 5)]
     ghost2 = Ghost(ghost2_init_node)
 
-    new_board.add_agent(pac_man)
-    new_board.add_agent(ghost1)
-    new_board.add_agent(ghost2)
+    # Add agents to game
+    new_game.add_pac_man(pac_man)
+    new_game.add_ghost(ghost1)
+    new_game.add_ghost(ghost2)
 
-    game_speed = 0
     nb_step = 100
-    play_games(new_board, nb_step, game_speed)
+    new_game.play_game(nb_step)
