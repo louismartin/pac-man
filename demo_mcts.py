f68182c28a5738bcd16e7f796f84d592586b7c78
Louis MARTIN <louisrtm@gmail.com>
2017-02-06 19:56:30 +0100

    Implement reward MCTS with discount factor on harder board with ghost

--- demo_mcts.py
+++ demo_mcts.py
@@ -3,12 +3,13 @@ from pacman.algorithms import MCTS
 from pacman.board import Board
 from pacman.game import Game
 
-board = Board('boards/3x3_board.txt')
+board = Board('boards/4x4_board.txt')
 game_speed = 0.01
-game = Game(board, game_speed, max_plays=10)
+game = Game(board, game_speed, max_plays=100)
 
 # Create agents and add them to game
 game.add("PacMan", position=(0, 0))
+game.add("Ghost", position=(3, 3))
 
-mcts = MCTS(game)
-mcts.train(train_time=10)
+mcts = MCTS(game, verbose=False)
+mcts.train(train_time=60, display_interval=500)
